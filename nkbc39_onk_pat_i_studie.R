811bd28033ff7949c4f799459823e15f3def9836
John Lövrot <john.lovrot@sll.se>
2018-08-13 14:54:54 +0200

    Första utkast till att kombinera nkbc24 och nkbc25

---
+++ nkbc39_onk_pat_i_studie.R
@@ -0,0 +1,72 @@
+NAME <- "nkbc39"
+
+GLOBALS <- defGlobals(
+  LAB = "Patienten ingår i studie",
+  POP = "opererade fall utan fjärrmetastaser vid diagnos.",
+  SJHKODUSE <- "post_inr_sjhkod"
+)
+
+dftemp <- addSjhData(dfmain)
+
+dftemp <- dftemp %>%
+  mutate(
+    # Hantera missing
+    pre_beh_studie = as.logical(ifelse(pre_beh_studie_Värde %in% c(0, 1), pre_beh_studie_Värde, NA)),
+    post_beh_studie = as.logical(ifelse(post_beh_studie_Värde %in% c(0, 1), post_beh_studie_Värde, NA)),
+    # Beräkna
+    outcome = ifelse(
+      prim_op == 1, post_beh_studie, 
+      ifelse(
+        !is.na(pre_inr_dat) & !is.na(post_inr_dat), pre_beh_studie | post_beh_studie, 
+        NA
+      )
+    )
+  ) %>%
+  filter(
+    # Reg av given onkologisk behandling
+    period >= 2012,
+
+    # ett år bakåt då info från onk behandling blanketter
+    period <= YEAR - 1,
+
+    # Endast opererade
+    !is.na(op_kir_dat),
+
+    # Ej fjärrmetastaser vid diagnos
+    !a_tnm_mklass_Värde %in% 10,
+
+    !is.na(region)
+  ) %>%
+  select(landsting, region, sjukhus, period, outcome, a_pat_alder, invasiv)
+
+link <- rccShiny(
+  data = dftemp,
+  folder = NAME,
+  path = OUTPUTPATH,
+  outcomeTitle = GLOBALS$LAB,
+  folderLinkText = GLOBALS$SHORTLAB,
+  geoUnitsPatient = FALSE,
+  textBeforeSubtitle = GLOBALS$SHORTPOP,
+  description = c(
+    "Ett övergripande mål är att erbjuda alla bröstcancerpatienter medverkan i studier för att utveckla nya behandlingar och arbetssätt. Detta gäller alla typer av studier (t.ex. kliniska, omvårdnad, fysioterapi).",
+    paste0(
+      onkRed,
+      "<p></p>",
+      descTolk
+    ),
+    descTekBes()
+  ),
+  varOther = list(
+    list(
+      var = "a_pat_alder",
+      label = c("Ålder vid diagnos")
+    ),
+    list(
+      var = "invasiv",
+      label = c("Invasivitet vid diagnos")
+    )
+  )
+)
+
+cat(link)
+# runApp(paste0("Output/apps/sv/",NAME))
