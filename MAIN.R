6bdedb40fb46e931ce94fc4fc6ffd9d1d2d6cace
John Lövrot <john.lovrot@sll.se>
2019-02-11 15:53:41 +0100

    Beräknade variabler till separat generell verktygsfunktion

--- MAIN.R
+++ MAIN.R
@@ -35,6 +35,7 @@ tackning_tbl <- readxl::read_excel("G:/Hsf/RCC-Statistiker/Brostcancer/Brostcanc
 # Bearbeta huvud-dataram
 dfmain <- df %>%
   mutate_if(is.factor, as.character) %>%
+  derive_nkbc_d_vars() %>%
   mutate(
     period = year(a_diag_dat), # Den period som appar visas för
     opyear = year(op_kir_dat),
@@ -53,140 +54,6 @@ dfmain <- df %>%
       REGION_NAMN == "Region Väst" ~ 5L,
       REGION_NAMN == "Region Norr" ~ 6L,
       TRUE ~ NA_integer_
-    ),
-
-    # Derivering av primär behandling
-    d_prim_beh_Värde = coalesce(op_kir_Värde, a_planbeh_typ_Värde),
-
-    # Derivering av invasiv cancer
-    d_invasiv_Värde = ifelse(d_prim_beh_Värde == 1, op_pad_invasiv_Värde,
-      ifelse(d_prim_beh_Värde %in% c(2, 3) | is.na(d_prim_beh_Värde), a_pad_invasiv_Värde,
-        NA
-      )
-    ),
-
-    d_invasiv_Värde = ifelse(d_invasiv_Värde == 98, NA, d_invasiv_Värde), ## added 2017-11-09
-    d_invasiv = factor(d_invasiv_Värde, c(1, 2, NA),
-      c("Invasiv cancer", "Enbart cancer in situ", "Uppgift saknas"),
-      exclude = NULL
-    ),
-
-    # Biologisk subtyp
-    # ER, 1 = pos, 2 = neg
-    d_er_op_Värde = case_when(
-      op_pad_erproc < 10 | is.na(op_pad_erproc) & op_pad_er_Värde %in% 2 ~ 2,
-      op_pad_erproc >= 10 | is.na(op_pad_erproc) & op_pad_er_Värde %in% 1 ~ 1
-    ),
-
-    d_er_a_Värde = case_when(
-      a_pad_erproc < 10 | is.na(a_pad_erproc) & a_pad_er_Värde %in% 2 ~ 2,
-      a_pad_erproc >= 10 | is.na(a_pad_erproc) & a_pad_er_Värde %in% 1 ~ 1
-    ),
-
-    d_er_Värde = ifelse(d_prim_beh_Värde == 1, d_er_op_Värde,
-      ifelse(d_prim_beh_Värde %in% c(2, 3), d_er_a_Värde,
-        NA
-      )
-    ),
-
-    # PR, 1 = pos, 2 = neg
-    d_pr_op_Värde = case_when(
-      op_pad_prproc < 10 | is.na(op_pad_prproc) & op_pad_pr_Värde %in% 2 ~ 2,
-      op_pad_prproc >= 10 | is.na(op_pad_prproc) & op_pad_pr_Värde %in% 1 ~ 1
-    ),
-
-    d_pr_a_Värde = case_when(
-      a_pad_prproc < 10 | is.na(a_pad_prproc) & a_pad_pr_Värde %in% 2 ~ 2,
-      a_pad_prproc >= 10 | is.na(a_pad_prproc) & a_pad_pr_Värde %in% 1 ~ 1
-    ),
-
-    d_pr_Värde = ifelse(d_prim_beh_Värde == 1, d_pr_op_Värde,
-      ifelse(d_prim_beh_Värde %in% c(2, 3), d_pr_a_Värde,
-        NA
-      )
-    ),
-
-    # HER2, 1 = pos, 2 = neg
-    d_her2_op_Värde = case_when(
-      op_pad_her2_Värde %in% 3 | op_pad_her2ish_Värde %in% 1 ~ 1,
-      op_pad_her2_Värde %in% c(1, 2) | op_pad_her2ish_Värde %in% 2 ~ 2
-    ),
-
-    d_her2_a_Värde = case_when(
-      a_pad_her2_Värde %in% 3 | a_pad_her2ish_Värde %in% 1 ~ 1,
-      a_pad_her2_Värde %in% c(1, 2) | a_pad_her2ish_Värde %in% 2 ~ 2
-    ),
-
-    d_her2_Värde = ifelse(d_prim_beh_Värde == 1, d_her2_op_Värde,
-      ifelse(d_prim_beh_Värde %in% c(2, 3), d_her2_a_Värde,
-        NA
-      )
-    ),
-
-    d_subtyp = factor(case_when(
-      d_er_Värde %in% 2 & d_pr_Värde %in% 2 & d_her2_Värde %in% 2 ~ 1,
-      is.na(d_er_Värde) | is.na(d_pr_Värde) | is.na(d_her2_Värde) ~ 99,
-      d_her2_Värde %in% 1 ~ 2,
-      d_er_Värde %in% 1 | d_pr_Värde %in% 1 ~ 3,
-      TRUE ~ 99
-    ),
-    labels = c("Trippel negativ", "HER2 positiv", "Luminal", "Uppgift saknas")
-    ),
-
-    # T
-    d_tstad = factor(
-      case_when(
-        a_tnm_tklass_Värde == 0 ~ 1,
-        a_tnm_tklass_Värde == 5 ~ 1,
-        a_tnm_tklass_Värde == 10 ~ 1,
-        a_tnm_tklass_Värde == 20 ~ 2,
-        a_tnm_tklass_Värde == 30 ~ 2,
-        a_tnm_tklass_Värde == 42 ~ 2,
-        a_tnm_tklass_Värde == 44 ~ 2,
-        a_tnm_tklass_Värde == 45 ~ 2,
-        a_tnm_tklass_Värde == 46 ~ 2,
-        a_tnm_tklass_Värde == 50 ~ 99,
-        is.na(a_tnm_tklass_Värde) ~ 99,
-        TRUE ~ NA_real_
-      ),
-      levels = c(1, 2, 99),
-      labels = c("<=20mm (T0/T1)", ">20mm (T2-T4)", "Uppgift saknas")
-    ),
-
-    # N
-    d_nstad = factor(
-      case_when(
-        a_tnm_nklass_Värde == 0 ~ 1,
-        a_tnm_nklass_Värde == 10 ~ 2,
-        a_tnm_nklass_Värde == 20 ~ 2,
-        a_tnm_nklass_Värde == 30 ~ 2,
-        a_tnm_nklass_Värde == 40 ~ 99,
-        is.na(a_tnm_nklass_Värde) ~ 99,
-        TRUE ~ NA_real_
-      ),
-      levels = c(1, 2, 99),
-      labels = c("Nej (N0)", "Ja (N1-N3)", "Uppgift saknas")
-    ),
-
-    # fix 1.sjukhus ansvarigt för rapportering av onkologisk behandling/2.onkologiskt sjukhus/3.anmälande sjukhus
-    d_onkpostans_sjhkod = coalesce(
-      as.numeric(post_inr_sjhkod),
-      as.numeric(op_onk_sjhkod),
-      as.numeric(a_onk_rappsjhkod),
-      as.numeric(a_onk_sjhkod),
-      as.numeric(a_inr_sjhkod)
-    ),
-    d_onkpreans_sjhkod = coalesce(
-      as.numeric(pre_inr_sjhkod),
-      as.numeric(op_onk_sjhkod),
-      as.numeric(a_onk_rappsjhkod),
-      as.numeric(a_onk_sjhkod),
-      as.numeric(a_inr_sjhkod)
-    ),
-    # fix 1) post onk sjukhus 2) pre onk sjukhus
-    d_onk_sjhkod = coalesce(
-      as.numeric(post_inr_sjhkod),
-      as.numeric(pre_inr_sjhkod)
     )
   ) %>%
   filter( # default vilka år som ska visas
