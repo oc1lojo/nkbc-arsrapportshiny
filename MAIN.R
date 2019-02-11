db152bd7e9860905459b74842fbb7698b02414e9
John Lövrot <john.lovrot@sll.se>
2019-02-11 14:22:53 +0100

    Lägg verktygsfunktioner i separat fil

--- MAIN.R
+++ MAIN.R
@@ -5,48 +5,13 @@ library(lubridate)
 library(shiny)
 library(rccShiny)
 
-# Läs in/Definera "konstanter" och hjälpfunktioner -----------------------------
+# Läs in/Definera "konstanter" och verktygsfunktioner -----------------------------
 
 YEAR <- 2017
 OUTPUTPATH <- "Output"
 
+source("nkbc-general.R", encoding = "utf8")
 source("beskTXT.R", encoding = "utf8")
-
-addSjhData <- function(df = dfmain, SJHKODUSE = GLOBALS$SJHKODUSE) {
-  names(df)[names(df) == SJHKODUSE] <- "sjhkod"
-
-  df <- df %>%
-    mutate(sjhkod = as.numeric(sjhkod)) %>%
-    left_join(sjukhuskoder, by = c("sjhkod" = "sjukhuskod")) %>%
-    mutate(
-      region = case_when(
-        region_sjh_txt == "Sthlm/Gotland" ~ 1L,
-        region_sjh_txt == "Uppsala/Örebro" ~ 2L,
-        region_sjh_txt == "Sydöstra" ~ 3L,
-        region_sjh_txt == "Syd" ~ 4L,
-        region_sjh_txt == "Väst" ~ 5L,
-        region_sjh_txt == "Norr" ~ 6L,
-        TRUE ~ NA_integer_
-      ),
-      region = ifelse(is.na(region), region_lkf, region),
-      landsting = substr(sjhkod, 1, 2) %>% as.integer(),
-      # Fulfix Bröstmottagningen, Christinakliniken Sh & Stockholms bröstklinik så hamnar i Stockholm
-      landsting = ifelse(sjhkod %in% c(97333, 97563), 10, landsting),
-      landsting = ifelse(landsting %in% c(
-        seq(10, 13),
-        seq(21, 28),
-        30,
-        seq(41, 42),
-        seq(50, 57),
-        seq(61, 65)
-        # seq(91,96)
-      ),
-      landsting,
-      NA
-      )
-    )
-  return(df)
-}
 
 # Läs in data ------------------------------------------------------------------
 
