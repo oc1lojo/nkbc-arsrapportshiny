c2e5ba416af90666a16e12bf276bc4a78483fd70
John Lövrot <john.lovrot@sll.se>
2019-02-14 09:15:48 +0100

    Definiera även nkbc33 i nkbcind

--- nkbc33_tackning_mot_cancerreg.R
+++ nkbc33_tackning_mot_cancerreg.R
@@ -1,14 +1,3 @@
-nkbc33_def <- list(
-  code = "nkbc33",
-  lab = "Täckningsgrad mot cancerregistret",
-  pop = "alla anmälda fall",
-  target_values = c(95, 99),
-  sjhkod_var = "a_inr_sjhkod",
-  om_indikatorn = "Anmälan till cancerregistret och anmälan till kvalitetsregistret är kombinerade och därmed undviks dubbelarbete.",
-  vid_tolkning = NULL,
-  teknisk_beskrivning = NULL
-)
-
 dftemp <- data.frame(
   region = c(
     rep(tackning_tbl$region, tackning_tbl$finns),
