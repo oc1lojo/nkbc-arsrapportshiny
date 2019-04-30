d27b52de5a17edd6a3aee8fa85b4c08dccd4d389
John Lövrot <john.lovrot@sll.se>
2019-04-30 14:11:27 +0200

    Kopiera text från SVF till operation

--- nkbcind/nkbc43-ledtid-misstanke-till-prim-beh.R
+++ nkbcind/nkbc43-ledtid-misstanke-till-prim-beh.R
@@ -38,7 +38,11 @@ nkbc43 <- list(
   other_vars = c("a_pat_alder", "d_invasiv", "d_prim_beh_op"),
   om_indikatorn =
     paste(
-      "<mark>Standardiserat vårdförlopp infördes 2016 för att säkra utredning och start av behandling till patienter i rimlig tid.</mark>"
+      "Standardiserat vårdförlopp infördes 2016 för att säkra utredning och start av behandling till patienter i rimlig tid.",
+      "För bröstcancer är tiden från välgrundad misstanke till start av behandling 28 kalenderdagar.",
+      "Av patienter som utreds för cancer bör 80% ha gjort det inom denna tidsperiod.",
+      "För ett antal patienter krävs mer avancerade utredningsmetoder för att nå diagnos vilket kan förlänga tiden till behandlingsstart.",
+      "Startpunkten för SVF har tolkats olika av vårdgivare vilket ger upphov till variation varför ledtiden skall tolkas med stor försiktighet."
     ),
   vid_tolkning = NULL,
   inkl_beskr_missca = TRUE,
