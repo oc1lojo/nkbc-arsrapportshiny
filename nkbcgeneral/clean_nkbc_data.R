1dca78eea4d6d0d55a2de88589b7427dbf2eafd3
John Lövrot <john.lovrot@sll.se>
2019-06-11 15:45:38 +0200

    Städa fritext-variabler från specialtecken

--- nkbcgeneral/clean_nkbc_data.R
+++ nkbcgeneral/clean_nkbc_data.R
@@ -1,5 +1,12 @@
 clean_nkbc_data <- function(x, ...) {
-  x %>%
+  x <- x %>%
     mutate_at(vars(ends_with("_Värde")), as.integer) %>%
-    mutate_at(vars(ends_with("sjhkod")), as.integer)
+    mutate_at(vars(ends_with("sjhkod")), as.integer) %>%
+    mutate_if(
+      is.character,
+      # städa fritext-variabler från specialtecken
+      function(y) gsub("[[:space:]]", " ", y)
+    )
+
+  return(x)
 }
