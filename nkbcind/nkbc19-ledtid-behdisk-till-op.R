5d14f04c96c3f13c7725a4423a50f7219877c6dd
John Lövrot <john.lovrot@sll.se>
2019-09-09 12:55:39 +0200

    Ändra målnivå från 75/90 till 80 enligt beslut i ELG

--- nkbcind/nkbc19-ledtid-behdisk-till-op.R
+++ nkbcind/nkbc19-ledtid-behdisk-till-op.R
@@ -23,7 +23,7 @@ nkbc19 <- list(
     )
   },
   prop_within_value = 14,
-  target_values = c(75, 90),
+  target_values = 80,
   sjhkod_var = "op_inr_sjhkod",
   other_vars = c("a_pat_alder", "d_invasiv"),
   om_indikatorn = "Standardiserat vårdförlopp infördes 2016 för att säkra utredning och vård till patienter i rimlig och säker tid.",
