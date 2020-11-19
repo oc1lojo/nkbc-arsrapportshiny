60d9fd22b9d0fb67d5952e359b6eef45985d2a4d
John Lövrot <john.lovrot@sll.se>
2020-11-19 10:23:20 +0100

    Lägg till 1:a version av publiceringsskript

---
+++ publish.R
@@ -0,0 +1,28 @@
+app_dirs <- c(
+  list.dirs(path = "sv", recursive = FALSE),
+  list.dirs(path = "en", recursive = FALSE)
+)
+
+n_apps <- length(app_dirs)
+
+for (i in 1:n_apps) {
+  print(paste0("Publicerar (", i, "/", n_apps, "): ", app_dirs[i], "..."))
+
+  suppressMessages(
+    suppressWarnings(
+      rsconnect::deployApp(
+        appDir = app_dirs[i],
+        # Namngivningskonvention: register_språk_app
+        appName = paste(
+          c("brostcancer", unlist(strsplit(app_dirs[i], split = .Platform$file.sep))),
+          collapse = "_"
+        ),
+        logLevel = "quiet",
+        forceUpdate = TRUE
+      )
+    )
+  )
+}
+
+# Lista alla publicerade webbapplikationer
+# rsconnect::applications()
