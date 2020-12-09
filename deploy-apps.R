ec2c7890e7f170e3668c97a0033962c73a94d7ad
John Lövrot <john.lovrot@sll.se>
2020-12-09 10:06:36 +0100

    Anpassat utifrån https://prestatistik.incanet.se/guide/rccShiny---RStudio-Connect.html#uppdatera-redan-publicerad-applikation

--- deploy-apps.R
+++ deploy-apps.R
@@ -1,22 +1,61 @@
-app_dirs <- c(
+# Anpassat utifrån
+# https://prestatistik.incanet.se/guide/rccShiny---RStudio-Connect.html#uppdatera-redan-publicerad-applikation
+
+library(dplyr)
+library(rsconnect)
+library(connectapi)
+
+# Prefix för register ----------------------------------------------------------
+globalRegisterPrefix <- "brostcancer"
+
+# API --------------------------------------------------------------------------
+connectServer <- "https://prestatistik.incanet.se"
+connectAPIKey <- "..."
+
+client <- connectapi::connect(
+  host = connectServer,
+  api_key = connectAPIKey
+)
+
+# Hämta lista på alla applikationer på servern och välj ut de där namnet innehåller prefix
+# (Observera att ens användare måste ha skrivrättigheter till de applikationer man önskas justera)
+tempListApplications <-
+  connectapi::get_content(client, limit = Inf, page_size = 999999) %>%
+  dplyr::filter(stringr::str_detect(name, globalRegisterPrefix))
+
+# Publicera (om) appar ---------------------------------------------------------
+
+tempListApps <- c(
   list.dirs(path = "sv", recursive = FALSE),
   list.dirs(path = "en", recursive = FALSE)
 )
 
-n_apps <- length(app_dirs)
+for (i in 1:length(tempListApps)) {
+  cat(paste0("Publicerar (", i, "/", length(tempListApps), "): ", tempListApps[i], "...\n"))
 
-for (i in 1:n_apps) {
-  cat(paste0("Publicerar (", i, "/", n_apps, "): ", app_dirs[i], "...\n"))
+  # Namngivningskonvention: register_språk_app
+  tempAppName <- paste(
+    c(globalRegisterPrefix, unlist(strsplit(tempListApps[i], split = .Platform$file.sep))),
+    collapse = "_"
+  )
+
+  # Kontrollera om en app med samma namn finns på servern redan, uppdatera den i sådana fall
+  tempDfAppId <- tempListApplications %>%
+    dplyr::filter(name %in% tempAppName) %>%
+    dplyr::arrange(id) %>%
+    dplyr::distinct(id)
+  if (nrow(tempDfAppId) > 0) {
+    tempAppId <- tempDfAppId$id
+  } else {
+    tempAppId <- NULL
+  }
 
   suppressMessages(
     suppressWarnings(
       rsconnect::deployApp(
-        appDir = app_dirs[i],
-        # Namngivningskonvention: register_språk_app
-        appName = paste(
-          c("brostcancer", unlist(strsplit(app_dirs[i], split = .Platform$file.sep))),
-          collapse = "_"
-        ),
+        appDir = tempListApps[i],
+        appName = tempAppName,
+        appId = tempAppId,
         logLevel = "quiet",
         forceUpdate = TRUE
       )
@@ -24,23 +63,32 @@ for (i in 1:n_apps) {
   )
 }
 
-# Lista alla publicerade webbapplikationer
-# rsconnect::applications()
 
-# # Startsida
+# Startsida --------------------------------------------------------------------
 # setwd(startsida_dir)
-#
-# # tempAppFiles <- stringr::str_subset(list.files(recursive = TRUE), "rsconnect", negate = TRUE)
-# tempAppFiles <- c("index.html", list.files("_libs", full.names = TRUE, recursive = TRUE))
-#
-# # Inspektera filer
-# tempAppFiles
-#
-# rsconnect::deployApp(
-#   appDir = ".",
-#   appFiles = tempAppFiles,
-#   appName = "brostcancer",
-#   appPrimaryDoc = "index.html",
-#   logLevel = "quiet",
-#   forceUpdate = TRUE
-# )
+
+tempAppFiles <- c("index.html", list.files("_libs", full.names = TRUE, recursive = TRUE))
+
+# Inspektera filer
+tempAppFiles
+
+# Kontrollera om ett dokument med samma namn finns på servern redan, uppdatera det i sådana fall
+tempDfAppId <- tempListApplications %>%
+  dplyr::filter(name %in% globalRegisterPrefix) %>%
+  dplyr::arrange(id) %>%
+  dplyr::distinct(id)
+if (nrow(tempDfAppId) > 0) {
+  tempAppId <- tempDfAppId$id
+} else {
+  tempAppId <- NULL
+}
+
+rsconnect::deployApp(
+  appDir = ".",
+  appFiles = tempAppFiles,
+  appName = globalRegisterPrefix,
+  appId = tempAppId,
+  appPrimaryDoc = "index.html",
+  logLevel = "quiet",
+  forceUpdate = TRUE
+)
